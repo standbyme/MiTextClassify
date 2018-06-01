0c9b28fe09fab689af57d313ef654a55a7263cbf
StandbyMe <stand-by-me@mail.com>
2018-06-01 20:22:41 +0800

    wordSimEmbedding

--- src/main/scala/StandbyMe/MiTextClassify/main.scala
+++ src/main/scala/StandbyMe/MiTextClassify/main.scala
@@ -3,6 +3,6 @@ package StandbyMe.MiTextClassify
 object main extends App {
   val word1 = "note"
   val word2 = "三星"
-  val score = client.wordSimEmbedding(word1, word2, null).getDouble("score")
-  println(score)
+  val score_opt = wordSimEmbedding(word1, word2)
+  println(score_opt)
 }
--- src/main/scala/StandbyMe/MiTextClassify/package.scala
+++ src/main/scala/StandbyMe/MiTextClassify/package.scala
@@ -7,4 +7,13 @@ package object MiTextClassify {
   val API_KEY = sys.env("API_KEY")
   val SECRET_KEY = sys.env("SECRET_KEY")
   val client = new AipNlp(APP_ID, API_KEY, SECRET_KEY)
+
+  def wordSimEmbedding(word1: String, word2: String): Option[Double] = {
+    try {
+      val score = client.wordSimEmbedding(word1, word2, null).getDouble("score")
+      Some(score)
+    } catch {
+      case _ => None
+    }
+  }
 }
