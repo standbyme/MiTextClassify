fdf5bac49581719d8b3fc0b9fdd13a8d1e44ca52
StandbyMe <stand-by-me@mail.com>
2018-06-03 14:58:18 +0800

    Array to Seq

--- src/main/scala/StandbyMe/MiTextClassify/main.scala
+++ src/main/scala/StandbyMe/MiTextClassify/main.scala
@@ -2,7 +2,7 @@ package StandbyMe.MiTextClassify
 
 object main extends App {
 
-  val topics = Array("娱乐", "家居", "健康", "国际", "教育", "社会", "游戏", "旅行", "时政", "民生", "科学", "科技", "财经", "房产", "体育", "历史", "时尚", "军事", "美食", "汽车", "星座", "情感", "育儿", "文化", "天气", "宗教")
+  val topics = Seq("娱乐", "家居", "健康", "国际", "教育", "社会", "游戏", "旅行", "时政", "民生", "科学", "科技", "财经", "房产", "体育", "历史", "时尚", "军事", "美食", "汽车", "星座", "情感", "育儿", "文化", "天气", "宗教")
   val threshold = 0.2
 
   def wordSimEmbeddingAdapter(word_with_probability: WordWithProbability, topic: Topic): Option[(WordWithProbability, SimEmbeddingScore)] = {
@@ -15,17 +15,17 @@ object main extends App {
     probability * score
   }
 
-  def topic_handler(word_with_probability_array: Array[WordWithProbability])(topic: Topic): (Topic, SimEmbeddingScore) = {
+  def topic_handler(word_with_probability_seq: Seq[WordWithProbability])(topic: Topic): (Topic, SimEmbeddingScore) = {
     println(topic)
 
-    val scores = word_with_probability_array.flatMap(wordSimEmbeddingAdapter(_, topic)).map(word_with_probability_and_score_handler)
+    val scores = word_with_probability_seq.flatMap(wordSimEmbeddingAdapter(_, topic)).map(word_with_probability_and_score_handler)
     val average = scores.sum / scores.length
     println(scores.length)
     (topic, average)
   }
 
-  def word_with_probability_array_handler(word_with_probability_array: Array[WordWithProbability]): Option[Topic] = {
-    val temp = topics.map(topic_handler(word_with_probability_array)).sortBy(_._2)
+  def word_with_probability_seq_handler(word_with_probability_seq: Seq[WordWithProbability]): Option[Topic] = {
+    val temp = topics.map(topic_handler(word_with_probability_seq)).sortBy(_._2)
     temp.foreach(println)
     val (topic, score) = temp.maxBy(_._2)
     println(s"topic $topic    score $score")
@@ -33,7 +33,7 @@ object main extends App {
     else None
   }
 
-  val word_with_probability_array = Array(
+  val word_with_probability_seq = Seq(
     ("医药",0.34692),
     ("药店",0.0579149),
     ("药品",0.0359799),
@@ -47,5 +47,5 @@ object main extends App {
 
   )
 
-  println(word_with_probability_array_handler(word_with_probability_array))
+  println(word_with_probability_seq_handler(word_with_probability_seq))
 }
