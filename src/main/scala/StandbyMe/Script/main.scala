e46cd59f6d436db8aabcaf4bdb851a26f17e8ab5
StandbyMe <stand-by-me@mail.com>
2018-06-03 21:32:37 +0800

    Add script

---
+++ src/main/scala/StandbyMe/Script/main.scala
@@ -0,0 +1,45 @@
+package StandbyMe.Script
+
+import StandbyMe.MiTextClassify.{Topic, WordWithProbability, wordWithProbability_seq_handler}
+
+import scala.io.Source
+import java.io.PrintWriter
+import java.io.File
+
+object main extends App {
+  type FirstBlock = Seq[String]
+  type SecondBlock = Seq[WordWithProbability]
+
+  val file = Source.fromFile("topic_words.lda.txt")
+  val lines = file.getLines
+
+  def line_to_wordWithProbability(line: String): WordWithProbability = {
+    val Array(word, probability_string) = line.split("\t")
+    (word, probability_string.toDouble)
+  }
+
+  def firstBlock_to_secondBlock(firstBlock: FirstBlock): SecondBlock = {
+    firstBlock
+      .drop(2)
+      .map(line_to_wordWithProbability)
+  }
+
+  val firstBlock_array = lines.grouped(12)
+
+  val writer = new PrintWriter(new File("output"))
+
+  def handler(m: (Seq[Topic], Int)): Unit = {
+    val (topics, index) = m
+    println(s"Output $index")
+    writer.println(topics)
+  }
+
+
+  firstBlock_array
+    .map(firstBlock_to_secondBlock)
+    .map(wordWithProbability_seq_handler)
+    .zipWithIndex
+    .foreach(handler)
+
+  writer.close()
+}
