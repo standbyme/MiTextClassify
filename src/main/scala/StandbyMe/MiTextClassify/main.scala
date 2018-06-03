8b24d980cfa193c0952ecf9d5d5c9de585366972
StandbyMe <stand-by-me@mail.com>
2018-06-03 11:25:15 +0800

    Probability

--- src/main/scala/StandbyMe/MiTextClassify/main.scala
+++ src/main/scala/StandbyMe/MiTextClassify/main.scala
@@ -3,20 +3,34 @@ package StandbyMe.MiTextClassify
 object main extends App {
   type Topic = String
   type Word = String
+  type SimEmbeddingScore = Double
   type Score = Double
+  type Probability = Double
+  type WordWithProbability = (Word, Probability)
   val topics = Array("娱乐", "家居", "健康", "国际", "教育", "社会", "游戏", "旅行", "时政", "民生", "科学", "科技", "财经", "房产", "体育", "历史", "时尚", "军事", "美食", "汽车", "星座", "情感", "育儿", "文化", "天气", "宗教")
   val threshold = 0.2
 
-  def topic_handler(words: Array[Word])(topic: Topic): (Topic, Score) = {
+  def wordSimEmbeddingAdapter(word_with_probability: WordWithProbability, topic: Topic): Option[(WordWithProbability, SimEmbeddingScore)] = {
+    val word = word_with_probability._1
+    wordSimEmbedding(word, topic).map((word_with_probability, _))
+  }
+
+  def word_with_probability_and_score_handler(word_with_probability_and_score: (WordWithProbability, SimEmbeddingScore)): Score = {
+    val ((_, probability), score) = word_with_probability_and_score
+    probability * score
+  }
+
+  def topic_handler(word_with_probability_array: Array[WordWithProbability])(topic: Topic): (Topic, SimEmbeddingScore) = {
     println(topic)
-    val scores = words.flatMap(wordSimEmbedding(_, topic))
+
+    val scores = word_with_probability_array.flatMap(wordSimEmbeddingAdapter(_, topic)).map(word_with_probability_and_score_handler)
     val average = scores.sum / scores.length
     println(scores.length)
     (topic, average)
   }
 
-  def words_handler(words: Array[Word]): Option[Topic] = {
-    val temp = topics.map(topic_handler(words)).sortBy(_._2)
+  def word_with_probability_array_handler(word_with_probability_array: Array[WordWithProbability]): Option[Topic] = {
+    val temp = topics.map(topic_handler(word_with_probability_array)).sortBy(_._2)
     temp.foreach(println)
     val (topic, score) = temp.maxBy(_._2)
     println(s"topic $topic    score $score")
@@ -24,7 +38,18 @@ object main extends App {
     else None
   }
 
-  val words = Array("投资", "资产", "报告", "证券", "净值", "组合", "期末", "管理人", "债券", "持有")
+  val word_with_probability_array = Array(
+    ("投资", 0.0708659),
+    ("资产", 0.0246454),
+    ("报告", 0.0229653),
+    ("证券", 0.0224541),
+    ("净值", 0.0207581),
+    ("组合", 0.0202303),
+    ("期末", 0.0190425),
+    ("管理人", 0.0163324),
+    ("债券", 0.0155643),
+    ("持有", 0.0149047)
+  )
 
-  println(words_handler(words))
+  println(word_with_probability_array_handler(word_with_probability_array))
 }
