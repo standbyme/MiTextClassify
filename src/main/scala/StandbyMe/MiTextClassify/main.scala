83cac86cde47b79cedf1b57966d60f857d5f03b6
StandbyMe <stand-by-me@mail.com>
2018-06-06 20:04:46 +0800

    comment useless code

--- src/main/scala/StandbyMe/MiTextClassify/main.scala
+++ src/main/scala/StandbyMe/MiTextClassify/main.scala
@@ -1,20 +1,20 @@
 package StandbyMe.MiTextClassify
 
-object main extends App {
-
-  val wordWithProbability_seq = Seq(
-    ("医药", 0.34692),
-    ("药店", 0.0579149),
-    ("药品", 0.0359799),
-    ("药房", 0.0339464),
-    ("处方", 0.0258046),
-    ("零售", 0.0198529),
-    ("国药", 0.0195491),
-    ("回扣", 0.0168899),
-    ("健康", 0.0164062),
-    ("连锁", 0.0152807)
-
-  )
-
-  println(wordWithProbability_seq_handler(wordWithProbability_seq))
-}
+//object main extends App {
+//
+//  val wordWithProbability_seq = Seq(
+//    ("医药", 0.34692),
+//    ("药店", 0.0579149),
+//    ("药品", 0.0359799),
+//    ("药房", 0.0339464),
+//    ("处方", 0.0258046),
+//    ("零售", 0.0198529),
+//    ("国药", 0.0195491),
+//    ("回扣", 0.0168899),
+//    ("健康", 0.0164062),
+//    ("连锁", 0.0152807)
+//
+//  )
+//
+//  println(wordWithProbability_seq_handler(wordWithProbability_seq))
+//}
